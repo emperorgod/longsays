feb2670c64764e4d21af75f9b4ac3ee69c6de646
longsays@gmail.com <longsays@gmail.com@0566547c-929d-4324-4702-e158eea09666>
2013-07-04 12:50:01 +0000

    index

--- trunk/test/index.php
+++ trunk/test/index.php
@@ -1,13 +1,3 @@
-<?php    
-$url = 'http://pan.baidu.com/share/link?shareid=' . $_GET['shareid'] . '&uk='. $_GET["uk"];    
-$src = file_get_contents($url);//获取下载页面源码   
-$pattern = '/<a class="new-dbtn"(.*?)href=(.*?) id="downFileButtom">/is';//匹配下载按钮   
-preg_match_all($pattern, $src, $result);   
-$tempurl = implode("", $result[2]);//各种转换   
-$fileurlt = str_replace("\"", "", $tempurl);//各种替换   
-$fileurl = str_replace("&amp;", "&", $fileurlt);   
-header("location:$fileurl");//Got it   
-?>
 <!---
          )               (   
         / \  .-"""""-.  / \ 
@@ -23,6 +13,16 @@ header("location:$fileurl");//Got it
           {             }                                       龙语视觉
            `"="="="="="`
 --->
+<?php    
+$url = 'http://pan.baidu.com/share/link?shareid=' . $_GET['shareid'] . '&uk='. $_GET["uk"];    
+$src = file_get_contents($url);//获取下载页面源码   
+$pattern = '/<a class="new-dbtn"(.*?)href=(.*?) id="downFileButtom">/is';//匹配下载按钮   
+preg_match_all($pattern, $src, $result);   
+$tempurl = implode("", $result[2]);//各种转换   
+$fileurlt = str_replace("\"", "", $tempurl);//各种替换   
+$fileurl = str_replace("&amp;", "&", $fileurlt);   
+header("location:$fileurl");//Got it   
+?>
 <html lang="zh-CN">
 <head>
 <meta charset="UTF-8" />
